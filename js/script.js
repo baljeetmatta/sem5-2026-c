ef59c9a462464f138d6fad507d8d3237f4a390a2
Baljeet <baljeet@Baljeets-MacBook-Air.local>
2026-06-25 12:24:48 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+//alert("Hello");
+// a=10;
+// console.log(typeof(a));
+// a="Test"
+// console.log(typeof(a));
+"use strict"
+
+// console.log(code);
+
+//  var code="Test";
+
+
+//  code="Programming";
+
+
+// console.log(code);
+
+// function test()
+// {
+//     console.log(a);
+
+//     if(4>1)
+//     {
+//         var a=10;
+//         console.log(a);
+
+//     }
+// console.log(a);
+
+// }
+
+// test();
+// console.log(a);
+
+// let a=10;
+
+// console.log(a);
+
+// let a=20;
+// console.log(a);
+
+// const a=10;
+// console.log(a);
+// a=20;
+
+// let obj={
+//     "name":"test"
+
+// }
+// console.log(obj);
+// obj.name="code";
+// console.log(obj);
+
+// const data=obj;
+// data.name="Another";
+// console.log(obj);
+// data=10;
+
+//No+No=sum
+//String+No=contact
+//console.log(true+true+false);
+//Number,parseInt
+let a="2basd3ssads";
+//let b=Number(a)+1;
+let b=parseInt(a)+1;
+console.log(b);
+
+
+
+
+
+
+
