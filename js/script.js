be5d2f7e32214ab9d3c74ef5fc213a905bdcb3fd
Baljeet <baljeet@Baljeets-MacBook-Air.local>
2026-06-29 12:15:55 +0530

    function, array

--- js/script.js
+++ js/script.js
@@ -60,14 +60,131 @@
 //String+No=contact
 //console.log(true+true+false);
 //Number,parseInt
-let a="2basd3ssads";
-//let b=Number(a)+1;
-let b=parseInt(a)+1;
-console.log(b);
+// let a="2basd3ssads";
+// //let b=Number(a)+1;
+// let b=parseInt(a)+1;
+// console.log(b);
+//1. Regular
+//2. Arrow
+// function test(x,y)
+// {
+// //    console.log(arguments[0],arguments[1],arguments[2]);
+
+//     // if(y==undefined)
+//     //     y=0;
+
+//    // console.log(x+y)
+//     return x+y;
+// }
+// let result=test(10,20);
+// console.log(result);
+
+
+//console.log(test());
+//console.log(test);
+
+
+//console.log(Number("1459b0a")+1);
+//Number+number=sum
+//String+number=concat
+//String+String=concat
+
+//console.log(x);
+
+//var x=10;
+
+// function test()
+// {
+//     console.log(x);
+
+// }
+// test();
+
+
+// function test()
+// {
+//     if(4>1)
+//     {
+//         var a=10;
+//         console.log(a);
+//     }
+//     console.log(a);
+// }
+// test();
+
+// console.log(a);
+
+
+//Function as Expression
+// function test()
+// {
+//     console.log("Test called")
+// }
+
+// let data=test;
+// //console.log(data);
+// data();
+// let code=function ()
+// {
+
+// }
+
+///Regular vs Arrow
+// console.log(sum());
+// function sum()
+// {
+//     return "Hello";
+// }
+
+// let add=()=>{
+//     return "Hello";
+
+// }
+
+
+// let add1=()=>"Hello";
+// let add2=()=>{
+//     return "Hello";
+// }
+
+// function sum(x,y){
+//     return x+y;
+// }
+
+// var add=(x,y)=>x+y;
+// console.log(add(3,4));
 
 
 
+// function service(x)
+// {
+
+//     console.log("Service being performed...")
+//     x();
 
 
 
+// }
+// function washing()
+// {
+// console.log("Washing called...")
+// }
+// function callme()
+// {
+//     console.log("Callme called...")
+
+// }
+
+// service(washing);
+
+// service(callme);
+
+// let arr=[12,45.6,"Demo",10];
+// //console.log(arr.length);
+// arr[20]=100;
+// console.log(arr);
+
+// let arr1=[];
+// arr1[5]=20;
+// console.log(arr1);
 
